5c766814647cb0e34a5817f0e611107e0936ae4b
Andrea Gauta <andreyta2001.santi@gmail.com>
2021-03-21 02:06:24 -0500

    programacion de sistemas

---
+++ index.php
@@ -0,0 +1,48 @@
+
+<html>
+		<head>
+			<title>TALLER 2</title>
+		</head>
+		<body>
+		 	<?php
+
+		 		
+		 		$variable1 = "Programacion de sistema  de informacion";
+
+		 			echo '<h1>La variable de tipo texto es:  '  .$variable1. '</h1>';
+		 			echo '<br>'; 
+
+
+
+		 		$variable2 = 3;
+
+		 			echo ' La variable de tipo numerico es:  ' .$variable2;
+		 			echo '<br>';
+
+		 		$variable3 = True;
+		 		
+
+		 			echo ' La variable de tipo boolean es:  ' .$variable3;
+		 			echo '<br>';
+
+	 			$variable4 = 3.1416;
+		 		
+
+		 			echo ' La variable de tipo double es:  ' .$variable4;
+		 			echo '<br>';
+
+				$variable5 = 'lunes';
+		 		
+
+		 			echo ' La variable de tipo date es:  ' .$variable5;
+		 			echo '<br>';
+
+		 		$variable6 = 'redes y sistemas';
+		 		
+
+		 			echo ' La etiqueta  h2 es: <h2> ' .$variable6. '</h2>';
+		 			echo '<br>';
+		 		
+		 	?>
+		</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,28 @@
+
+<html>
+	<head>
+		<title>Taller 2</title>
+	</head>
+	<body>
+			<?php
+
+					$array[0] = "19";
+
+					$array[1] = "12";
+
+					$array[2] = "45";
+
+					$array[3] = "39";
+
+					$array[4] = "23";
+
+					
+
+					for($i=0;$i<count($array);$i++) {
+
+					echo 'Array['.$array[$i].']<br />';
+				}
+
+			?>
+	</body>
+</html>
